35bd085af87f1d0ccd9a043cff120e82e74e3ee8
d0-n <d.marume@alustudent.com>
2026-02-25 19:48:00 +0200

    Level 5

---
+++ level5_quests/quest_24_master_spell.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def ask_for_age():
+    age = int(input("What is your age? "))
+    return age
+
+def can_they_vote(age):
+    if age >= 18:
+        print("You can vote!")
+    else:
+        print("You cannot vote yet.")
+
+user_age = ask_for_age()
+can_they_vote(user_age)
